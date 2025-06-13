14e1570ba512b059bc3df907db75027ce3cff912
ifpb <ifpb@DSK11LB02-JP>
2025-06-13 14:01:39 -0300

    contato.html abrindo

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   base: '/Flor-de-Mandacaru/',
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        contato: 'contato.html',
+      },
+    },
+  },
+});
